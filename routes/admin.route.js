459b9e9ee79a0551c2c202a8917a134472279bcd
Canh <canh.pham@spiraledge.com>
2023-05-24 20:04:43 +0700

    update password changing for admin account

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const auth = require('../Controllers/authAdmin.controller')
+const verifyToken = require('../middleware/verifyToken')
+const verifyAdmin = require('../middleware/verifyAdmin')
 const router = express.Router()
 
 router.post('/signup', auth.signUp)
 router.post('/login', auth.login)
+router.post('/changePassword', verifyToken, verifyAdmin, auth.changePassword)
 
 module.exports = router

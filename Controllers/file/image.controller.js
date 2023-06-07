05e7baee570ed20ead43f55976e3e7a76e8fc7b8
Quy <frwk.HoangQuy@gmail.com>
2023-06-07 20:47:38 +0700

    test

--- Controllers/file/image.controller.js
+++ Controllers/file/image.controller.js
@@ -6,50 +6,56 @@ const mime = require('mime-types');
 
 const image = { 
     upload: async (req, res) => {
-        const file = req.file;
-        const imageId = req.body.imageId; // Retrieve the image ID from the form
-    
-        let connection
-    
-            if (!file) {
-            return res.status(400).send('No image uploaded.');
-            }
-              
-              connection = await DB.getConnection();
-    
-              await connection.beginTransaction();
-    
-              // Update the image file in the MySQL database
-            try {
-    
-                const result = await connection.query(
-                    'INSERT INTO images (filepath) VALUES (?)',
-                    [file.path]
-                );
-    
-                await connection.commit();
-            
-                if(result[0].affectedRows === 1){
-                    res.status(200).json({msg : 'Image uploaded successfully.'});
-                }
-            } catch (error) {
-                console.log(error)
-    
-                if (connection) {
-                    await connection.rollback();
-                  }
-                  fs.unlinkSync(file.path);
-    
-                  return res.status(500).json({ msg: 'Server Error' });
-            } finally {
-                
-                 // Release the connection back to the pool
-              if (connection) {
-                connection.release();
-              }
-            }
+        const files = req.files; // Retrieve the array of uploaded files
+        const imageIds = req.body.imageIds; // Retrieve the array of image IDs from the form
       
-    },
+        let connection;
+      
+        if (!files || files.length === 0) {
+          return res.status(400).send('No images uploaded.');
+        }
+      
+        connection = await DB.getConnection();
+      
+        await connection.beginTransaction();
+      
+        try {
+          for (let i = 0; i < files.length; i++) {
+            const file = files[i];
+            const imageId = imageIds[i];
+      
+            const result = await connection.query(
+              'UPDATE images SET filepath = ? WHERE id = ?',
+              [file.path, imageId]
+            );
+            
+            if (result[0].affectedRows !== 1) {
+              throw new Error(`Failed to update image with ID: ${imageId}`);
+            }
+          }
+      
+          await connection.commit();
+      
+          res.status(200).json({ msg: 'Images updated successfully.' });
+        } catch (error) {
+          console.log(error);
+      
+          if (connection) {
+            await connection.rollback();
+          }
+      
+          files.forEach((file) => {
+            fs.unlinkSync(file.path);
+          });
+      
+          return res.status(500).json({ msg: 'Server Error' });
+        } finally {
+          if (connection) {
+            connection.release();
+          }
+        }
+      },
+      
     get: async (req, res) => {
         const imageName = req.params.imageName;
         const imagePath = path.join('uploads/', imageName);
